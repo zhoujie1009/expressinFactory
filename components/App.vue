7cb5e6cf424730db9ef1f73348e2b850c8782f6e
zhoujie <zhoujie@qiyi.com>
2017-01-05 20:05:35 +0800

    template optimize

--- components/App.vue
+++ components/App.vue
@@ -2,12 +2,11 @@
     <div class="wrapper">
         <div class="nav"><span class="red_font">Expression</span>Factory</div>
         <div class="content_wrap">
-            <Sidertool></Sidertool>
             <div class="main_content">
                 <Headtool></Headtool>
                 <Editarea></Editarea>
-                <Foottool></Foottool>
             </div>
+            <Sidertool></Sidertool>
         </div>
     </div>
 </template>
@@ -17,14 +16,12 @@
 import Sidertool from './Sidertool.vue'
 import Headtool from './Headtool.vue'
 import Editarea from './Editarea.vue'
-import Foottool from './Foottool.vue'
 
 export default {    
   components: {
     Sidertool,
     Headtool,
-    Editarea,
-    Foottool
+    Editarea
   }
 }
 </script>
--- components/Sidertool.vue
+++ components/Sidertool.vue
@@ -1,75 +1,47 @@
 <template>
-	<div class="sider_bar">
-      <span class="bar_item bar_active importImg">
-        <input type="file" id="addImg" class="addImg" @change="setImg()">
-      </span>
-	    <span class="bar_item" @click="rasterize()"><i class="fa fa-sign-out mr18"></i>导出</span>
-	    <span class="bar_item"><i class="fa fa-floppy-o mr18"></i></i>保存</span>
-	</div>
-</template>
-<script>
-    export default{
-        data(){
-            return {
+    <div class="magic_area">
+        <div class="setting_tab">
+            <span 
+                class="tab_item"
+                v-for="tab in tabs"
+                :class="{'tab_active': $index === selected}"
+                @click="choose($index)">{{tab.tabName}}</span>
+        </div>
+        <component :is="currentView" transition="fade" transition-mode="out-in"></component>
 
-            }
-        },
-        methods:{
-            setImg(){
-                var imgUrl = this.getObjectURL(document.getElementById('addImg').files[0]);
-                var randomVal1 = Math.random();
-                var randomVal2 = Math.random();
-                this.addImage(imgUrl,Math.min(randomVal1, randomVal2),Math.max(randomVal1, randomVal2));
-            },
-            getObjectURL(file) {
-                var url = null;
-                if (window.createObjectURL != undefined) {
-                    url = window.createObjectURL(file)
-                } else if (window.URL != undefined) {
-                    url = window.URL.createObjectURL(file)
-                } else if (window.webkitURL != undefined) {
-                    url = window.webkitURL.createObjectURL(file)
-                }
-                return url
-            },
-            addImage(imgAdd, minScale, maxScale) {
-                var _this = this;
-                var coord = this.getRandomLeftTop();
-                fabric.Image.fromURL(imgAdd, function(image) {
-                  image.set({
-                    left: coord.left,
-                    top: coord.top,
-                    type: 'image',
-                    angle: fabric.util.getRandomInt(-10, 10)
-                  })
-                  .scale(_this.getRandomNum(minScale, maxScale))
-                  .setCoords();
-                  canvas.add(image);
-                });
-                this.uploadTime = 0;
-            },
-            getRandomNum(min, max) {
-                return Math.random() * (max - min) + min;
-            },
-            getRandomLeftTop() {
-                var offset = 50;
-                return {
-                    left: fabric.util.getRandomInt(0 + offset, 700 - offset),
-                    top: fabric.util.getRandomInt(0 + offset, 500 - offset)
-                };
-            },
-            rasterize() {
-                if (!fabric.Canvas.supports('toDataURL')) {
-                    alert('This browser doesn\'t provide means to serialize canvas to an image');
-                }else {
-                    window.open(canvas.toDataURL('png'));
-                }
-            },
-            rasterizeSVG() {
-                window.open(
-                  'data:image/svg+xml;utf8,' +
-                  encodeURIComponent(canvas.toSVG()));
-            }
+    </div>
+</template>
+
+<script> 
+import Basicsetone from './Basicsetone.vue'
+import Basicsettwo from './Basicsettwo.vue'
+import Basicsetthree from './Basicsetthree.vue'
+import Basicsetfour from './Basicsetfour.vue'
+
+export default{
+    data(){
+        return{
+            tabs:[
+                {tabName:"图片效果"},
+                {tabName:"高级效果"},
+                {tabName:"基本设置3"},
+                {tabName:"基本设置4"}
+            ],
+            selected:0,
+            currentView:'set_tab_0'
         }
+    },
+    methods:{
+        choose(index){
+            this.selected = index;
+            this.currentView = 'set_tab_' + index;
+        }
+    },
+    components:{
+        'set_tab_0':Basicsetone,
+        'set_tab_1':Basicsettwo,
+        'set_tab_2':Basicsetthree,
+        'set_tab_3':Basicsetfour
     }
+}
 </script>
